a834ed2e2b3e80cc965507280fc3139159c2728e
Kiarash Golzadeh <kiarash.gl@gmail.com>
2018-12-18 18:44:18 +0330

    second version

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Post
+
+
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html',{'posts' : Post.objects.all()})
