087a88406aee9d8dcf9b8133928f59cb2f2132a8
Kiarash Golzadeh <kiarash.gl@gmail.com>
2019-05-27 18:13:49 +0430

    changed staging files updating git ignore

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,5 @@ from blog.models import Post
 
 
 def index(request):
-    return render(request,'index.html',{'posts' : Post.objects.all()})
+    return render(request, 'index.html', {'posts': Post.objects.all(),
+                                          'about': "سلام. من کیارش هستم و این سایت رو برای معرفی رپ فارس به جهان موسیقی راه اندازی کردم."})
